45c4257381263a8313352694d95b2e4e8ccc5b72
“Keegan <keeganmoore.km@gmail.com>
2016-02-03 21:28:14 +0000

    Moving Parallel source code out of ADS1115 dev branch

--- rpi/parallel/doc/html/search/all_6d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['main',['main',['../parallel__io_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;parallel_io.c'],['../parallel__io__master_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;parallel_io_master.c'],['../parallel__io__slave_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;parallel_io_slave.c']]]
-];
--- rpi/parallel/doc/html/search/files_70.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['parallel_5fio_2ec',['parallel_io.c',['../parallel__io_8c.html',1,'']]],
-  ['parallel_5fio_5fmaster_2ec',['parallel_io_master.c',['../parallel__io__master_8c.html',1,'']]],
-  ['parallel_5fio_5fslave_2ec',['parallel_io_slave.c',['../parallel__io__slave_8c.html',1,'']]]
-];
