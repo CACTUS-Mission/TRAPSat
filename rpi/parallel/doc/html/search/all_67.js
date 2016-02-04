90b1238f0f2bdf0ac30931dacff8d011cc4ddc94
“Keegan <keeganmoore.km@gmail.com>
2016-02-04 02:09:19 +0000

    removed ads1115 and parallel stuff from main development branch

--- rpi/parallel/doc/html/search/all_67.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['gpio_5fdata',['GPIO_DATA',['../parallel__io__master_8c.html#ac6e63c89cb091f7d69042f595ed97ad7',1,'GPIO_DATA():&#160;parallel_io_master.c'],['../parallel__io__slave_8c.html#a24489bd94ed852195f66667b4e3e416e',1,'GPIO_DATA():&#160;parallel_io_slave.c']]],
-  ['gpio_5fread',['gpio_read',['../parallel__io__slave_8c.html#acfd5dc4c81af8cff281c36da3d1f38ba',1,'parallel_io_slave.c']]],
-  ['gpio_5fread_5fstrobe',['GPIO_READ_STROBE',['../parallel__io__master_8c.html#a87f993064bfdbb948413eee304430857',1,'GPIO_READ_STROBE():&#160;parallel_io_master.c'],['../parallel__io__slave_8c.html#a87f993064bfdbb948413eee304430857',1,'GPIO_READ_STROBE():&#160;parallel_io_slave.c']]],
-  ['gpio_5fwrite',['gpio_write',['../parallel__io__slave_8c.html#ae34db764eae9fd4ea46320c832bae616',1,'parallel_io_slave.c']]]
-];
