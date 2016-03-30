3d213c88862e161736c8a9aeabdf77a983f7920b
Keegan Moore <keeganmoore.km@gmail.com>
2016-03-29 20:33:46 -0400

    moved sendfilenametotim call

--- TRAPSat_cFS/cfs/apps/vc0706/fsw/src/vc0706_device.c
+++ TRAPSat_cFS/cfs/apps/vc0706/fsw/src/vc0706_device.c
@@ -138,11 +138,12 @@ int VC0706_takePics(void)
 		    }
 		    else
 		    {
-			//OS_printf("VC0706: Wrote Picture Filename to HK Packet. Sent: '%.*s'\n", 15, (char * )&pic_file_name[12], hk_packet_succes);
-		    }
+			  //OS_printf("VC0706: Wrote Picture Filename to HK Packet. Sent: '%.*s'\n", 15, (char * )&pic_file_name[12], hk_packet_succes);
+		      VC0706_SendTimFileName(file_name);
+            }
 		    //OS_printf("VC0706: VC0706_HkTelemetryPkt.vc0706_filename: '%s'\n", VC0706_HkTelemetryPkt.vc0706_filename);
 
-            VC0706_SendTimFileName(file_name);
+            
 
 		    /*
 		    ** incriment num pics for filename
