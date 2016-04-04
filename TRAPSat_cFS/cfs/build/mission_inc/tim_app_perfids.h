e71ca6a197c6fd256af9c28259f680c93f5a95fe
BigTuna94 <zach.richard94@gmail.com>
2016-04-04 23:08:45 +0000

    fixed tracking of *.o and *.so files. they are no longer tracked.

---
+++ TRAPSat_cFS/cfs/build/mission_inc/tim_app_perfids.h
@@ -0,0 +1,21 @@
+/************************************************************************
+** File:
+**   $Id: tim_app_perfids.h  $
+**
+** Purpose: 
+**  Define TRAPSat Interface Manager App Performance IDs
+**
+** Notes:
+**
+*************************************************************************/
+#ifndef _tim_app_perfids_h_
+#define _tim_app_perfids_h_
+
+
+#define TIM_APP_PERF_ID              98 
+
+#endif /* _tim_app_perfids_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
