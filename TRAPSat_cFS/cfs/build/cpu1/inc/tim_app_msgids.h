d5e87bd10b0a81b153c1c2e608ad1f1c36b19b77
BigTuna94 <zach.richard94@gmail.com>
2016-04-05 17:15:36 +0000

    fixed tracking .o and .so files -- no longer tracked

---
+++ TRAPSat_cFS/cfs/build/cpu1/inc/tim_app_msgids.h
@@ -0,0 +1,30 @@
+/************************************************************************
+** File:
+**   $Id: tim_app_msgids.h  $
+**
+** Purpose: 
+**  Define TIM App  Message IDs
+**
+** Notes:
+**
+**
+*************************************************************************/
+#ifndef _tim_app_msgids_h_
+#define _tim_app_msgids_h_
+
+/*
+** not in this version of the cFS
+#include "mission_msgids.h"
+#include "platform_base_msgids.h"
+*/
+
+#define TIM_APP_CMD_MID            0x188A	/* CFE_CMD_MID_BASE + TIM_APP_CMD_MID_OFFSET */
+#define TIM_APP_SEND_HK_MID        0x188B	/* CFE_CMD_MID_BASE + TIM_APP_SEND_HK_MID_OFFSET */
+#define TIM_APP_HK_TLM_MID         0x088B   /* CFE_TLM_MID_BASE + TIM_APP_HK_TLM_MID_OFFSET */
+
+
+#endif /* _tim_app_msgids_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
