a1cbf567a03ec8f73f14ecd77b53236d38191162
Ezra Brooks <ezra.m.brooks@gmail.com>
2016-05-01 16:57:05 -0400

    First cleanup and documentation pass on read_parallel
    To explain my thought process on adding PIN_COUNT:
    1. Magic numbers are bad, so having "7" as our loop condition is not
    very readable.
    2. If someone ever changes the pin array(s) without changing PIN_COUNT,
    the compiler SHOULD warn them, preventing silent failure in the form of
    not reading pins past the 8th array element.

--- Arduino/read_parallel/read_parallel.ino
+++ Arduino/read_parallel/read_parallel.ino
@@ -1,38 +1,48 @@
-// Yun
-//int parallel_read_pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+/*
+  read_parallel.ino - Reads pins in parallel.
+*/
 
-// Mega
-int parallel_read_pins[] = {31, 33, 35, 37, 39, 41, 43, 45};
+// Number of pins that will be read
+const int PIN_COUNT = 8;
 
+// Pin numbers for Arduino Yún
+//int parallel_read_pins[PIN_COUNT] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+// Pin numbers for Arduino Mega
+int parallel_read_pins[PIN_COUNT] = {31, 33, 35, 37, 39, 41, 43, 45};
+
+// Initialize message strings for read loop
+String msg = "";
+String msg_next;
 
 void setup() {
-
-  for(int i=0; i<8; i++)
+  // Set pin mode for each pin to INPUT
+  for(int i = 0; i < PIN_COUNT; i++)
   {
     pinMode(parallel_read_pins[i], INPUT);
   }
-
+  
+  // Begin serial communication at 19.2k baud
   Serial.begin(19200);
-  while( !Serial ) ; // wait for serial terminal to be opened.
+  // Wait for serial connection to initialize
+  while(!Serial);
 }
 
-String msg = "";
-String msg_next = "";
-
 void loop() {
-  
+  // Reset msg_next for each read
   msg_next = "";
-  for(int i=0; i<8; i++)
+  
+  // Read all pins and append their value to msg_next
+  for(int i = 0; i < PIN_COUNT; i++)
   {
     msg_next += digitalRead(parallel_read_pins[i]); 
   }
 
+  // If data is different from last read, write to serial
   if(msg_next != msg)
   {
     msg = msg_next;
     //Serial.println(msg_next);
     Serial.println(msg);
   }
-
 }
-
